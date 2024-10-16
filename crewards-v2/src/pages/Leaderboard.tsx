4a86e0161da6d40e4841d40705790a63d2d31e49
sdragonpt <sergionunoribeiro@gmail.com>
2024-10-16 20:19:41 +0100

    Fixes

--- crewards-v2/src/pages/Leaderboard.tsx
+++ crewards-v2/src/pages/Leaderboard.tsx
@@ -8,24 +8,50 @@ function Leaderboard() {
   return (
     <div className="relative bg-cover bg-center bg-[#171414] overflow-hidden">
       {/* Imagens para alternar entre Empire e Shuffle */}
-      <div className="absolute top-48 lg:top-[10.6rem] 3xl:top-60 left-1/2 transform -translate-x-1/2 z-20 flex space-x-8 justify-center">
-        {/* Imagem do Empire */}
-        <img
-          src="/csgoempire.png"
-          alt="Empire"
-          className={`w-32 lg:w-28 3xl:w-36 cursor-pointer transition duration-300 ${
-            activeImage === 1 ? "brightness-100" : "brightness-50"
-          }`}
-          onClick={() => setActiveImage(1)} // Mudar para Empire ao clicar
-        />
-        {/* Imagem do Shuffle */}
-        <img
-          src="/shuffle.png"
-          alt="Shuffle"
-          className={`w-32 lg:w-28 3xl:w-36 cursor-pointer transition duration-300 ${
-            activeImage === 2 ? "brightness-100" : "brightness-50"
-          }`}
-          onClick={() => setActiveImage(2)} // Mudar para Shuffle ao clicar
+      <div className="absolute top-48 lg:top-[10.6rem] 3xl:top-60 left-1/2 transform -translate-x-1/2 z-20 flex flex-col items-center">
+        <div className="flex space-x-8 justify-center">
+          {/* Contêiner para a imagem do Empire */}
+          <div className="relative flex flex-col items-center">
+            <div
+              className={`rounded-xl px-3 transition-transform duration-300 transform ${
+                activeImage === 1 ? "" : "opacity-50"
+              }`}
+            >
+              <img
+                src="/csgoempire.png"
+                alt="Empire"
+                className={`w-32 lg:w-28 3xl:w-36 cursor-pointer transition duration-300 ease-in-out hover:scale-110`}
+                onClick={() => setActiveImage(1)} // Mudar para Empire ao clicar
+              />
+            </div>
+          </div>
+
+          {/* Contêiner para a imagem do Shuffle */}
+          <div className="relative flex flex-col items-center">
+            <div
+              className={`rounded-xl px-3 transition-transform duration-300 transform ${
+                activeImage === 2 ? "" : "opacity-50"
+              }`}
+            >
+              <img
+                src="/shuffle.png"
+                alt="Shuffle"
+                className={`w-32 lg:w-28 3xl:w-36 cursor-pointer transition duration-300 ease-in-out hover:scale-110`}
+                onClick={() => setActiveImage(2)} // Mudar para Shuffle ao clicar
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Linha de fundo unificada que se move */}
+        <div
+          className={`absolute bottom-0 h-1 transition-all duration-300 ease-in-out`}
+          style={{
+            left: activeImage === 1 ? "-3%" : "50%", // Prolonga a linha à esquerda para Empire
+            width: activeImage === 1 ? "53%" : "53%", // Aumenta a largura da linha para Empire
+            backgroundColor:
+              activeImage === 1 ? "rgb(234, 179, 13)" : "rgb(131, 55, 216)", // Amarelo para Empire e roxo para Shuffle
+          }}
         />
       </div>
 
--- crewards-v2/src/pages/Vip.tsx
+++ crewards-v2/src/pages/Vip.tsx
@@ -34,42 +34,51 @@ function Vip() {
       <div className="absolute inset-0 bg-black opacity-20 z-10" />
 
       {/* Imagens para alternar entre Empire e Shuffle */}
-      <div className="absolute top-48 lg:top-[10.6rem] 3xl:top-60 left-1/2 transform -translate-x-1/2 z-20 flex space-x-8 justify-center">
-        {/* Contêiner para a imagem do Empire */}
-        <div className="relative flex flex-col items-center">
-          <div
-            className={`bg-zinc-900 border-b-2 border-zinc-800 rounded-lg px-2 transition-transform duration-300 transform ${
-              activeImage === 1 ? "scale-105" : "opacity-40"
-            }`}
-          >
-            <img
-              src="/csgoempire.png"
-              alt="Empire"
-              className={`w-32 lg:w-28 3xl:w-36 cursor-pointer transition duration-300 ease-in-out hover:scale-110 ${
-                activeImage === 1 ? "brightness-100" : "brightness-100"
+      <div className="absolute top-48 lg:top-[10.6rem] 3xl:top-60 left-1/2 transform -translate-x-1/2 z-20 flex flex-col items-center">
+        <div className="flex space-x-8 justify-center">
+          {/* Contêiner para a imagem do Empire */}
+          <div className="relative flex flex-col items-center">
+            <div
+              className={`rounded-xl px-3 transition-transform duration-300 transform ${
+                activeImage === 1 ? "" : "opacity-50"
               }`}
-              onClick={() => handleImageClick(1)} // Mudar para Empire ao clicar
-            />
+            >
+              <img
+                src="/csgoempire.png"
+                alt="Empire"
+                className={`w-32 lg:w-28 3xl:w-36 cursor-pointer transition duration-300 ease-in-out hover:scale-110`}
+                onClick={() => handleImageClick(1)} // Mudar para Empire ao clicar
+              />
+            </div>
+          </div>
+
+          {/* Contêiner para a imagem do Shuffle */}
+          <div className="relative flex flex-col items-center">
+            <div
+              className={`rounded-xl px-3 transition-transform duration-300 transform ${
+                activeImage === 2 ? "" : "opacity-50"
+              }`}
+            >
+              <img
+                src="/shuffle.png"
+                alt="Shuffle"
+                className={`w-32 lg:w-28 3xl:w-36 cursor-pointer transition duration-300 ease-in-out hover:scale-110`}
+                onClick={() => handleImageClick(2)} // Mudar para Shuffle ao clicar
+              />
+            </div>
           </div>
         </div>
 
-        {/* Contêiner para a imagem do Shuffle */}
-        <div className="relative flex flex-col items-center">
-          <div
-            className={`bg-zinc-900 border-b-2 border-zinc-800 rounded-lg px-2 transition-transform duration-300 transform ${
-              activeImage === 2 ? "scale-105" : "opacity-40"
-            }`}
-          >
-            <img
-              src="/shuffle.png"
-              alt="Shuffle"
-              className={`w-32 lg:w-28 3xl:w-36 cursor-pointer transition duration-300 ease-in-out hover:scale-110 ${
-                activeImage === 2 ? "brightness-100" : "brightness-100"
-              }`}
-              onClick={() => handleImageClick(2)} // Mudar para Shuffle ao clicar
-            />
-          </div>
-        </div>
+        {/* Linha de fundo unificada que se move */}
+        <div
+          className={`absolute bottom-0 h-1 transition-all duration-300 ease-in-out`}
+          style={{
+            left: activeImage === 1 ? "-3%" : "50%", // Prolonga a linha à esquerda para Empire
+            width: activeImage === 1 ? "53%" : "53%", // Aumenta a largura da linha para Empire
+            backgroundColor:
+              activeImage === 1 ? "rgb(234, 179, 13)" : "rgb(131, 55, 216)", // Amarelo para Empire e roxo para Shuffle
+          }}
+        />
       </div>
 
       {/* Rotas para Empire e Shuffle */}
