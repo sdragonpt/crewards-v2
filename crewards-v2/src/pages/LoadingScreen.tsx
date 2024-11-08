61f975fa99b50dcd4ea84401c35b6b9bf1bf0c73
sdragonpt <sergionunoribeiro@gmail.com>
2024-11-08 12:42:25 +0000

    Updated Loading Screen

--- crewards-v2/src/pages/LoadingScreen.tsx
+++ crewards-v2/src/pages/LoadingScreen.tsx
@@ -1,27 +1,41 @@
 import React, { useEffect, useState } from "react";
 import "../css/LoadingScreen.css";
 
-const LoadingScreen: React.FC<{ isLoading: boolean }> = ({ isLoading }) => {
+const LoadingScreen: React.FC = () => {
   const [opacity, setOpacity] = useState(1);
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
 
   useEffect(() => {
-    if (!isLoading) {
-      // Waits 1 sec to do the transition
-      const timer = setTimeout(() => {
-        setOpacity(0);
-      }, 1000); // 1 sec
+    // Evento para verificar quando a página estiver completamente carregada
+    const handlePageLoad = () => {
+      setIsPageLoaded(true); // Define que a página foi carregada
+    };
 
+    // Adiciona o ouvinte para o evento de load da janela
+    window.addEventListener("load", handlePageLoad);
+
+    return () => {
+      // Remove o ouvinte ao desmontar o componente
+      window.removeEventListener("load", handlePageLoad);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isPageLoaded) {
+      // Aguarda 1 segundo para garantir o tempo mínimo de exibição
+      const timer = setTimeout(() => {
+        setOpacity(0); // Inicia a transição para opacidade 0 após 1 segundo
+      }, 1000); // 1 segundo
+
+      // Limpa o timer ao desmontar ou alterar o estado
       return () => clearTimeout(timer);
-    } else {
-      // Reset opacity after load
-      setOpacity(1);
     }
-  }, [isLoading]);
+  }, [isPageLoaded]); // Executa quando a página estiver carregada
 
   return (
     <div
       className="loading-screen z-50"
-      style={{ opacity, pointerEvents: isLoading ? 'auto' : 'none' }} // No interaction while still loading
+      style={{ opacity, pointerEvents: isPageLoaded ? "none" : "auto" }} // Desabilita interação após o carregamento
     >
       <img
         className="footer-logo w-32 glow-effect-3"
